2758de9c94cca4d2e92396bc4aa99137694ecc25
joshuajeschek <joshua.jeschek@gmail.com>
2022-01-24 21:31:59 +0100

    [fix] enforce modifer naming

--- src/parser/util.ts
+++ src/parser/util.ts
@@ -15,26 +15,26 @@ export enum Modifiers {
 /**
  * Extracts modifiers
  * @param input String that starts with modifiers / already split string
- * @returns all modifiers as modifiers and the new, unflagged string
+ * @returns all modifiers as modifiers and the new string (without modifiers)
  */
 export function getModifiers(input: string[] | string): { modifiers: Modifiers[]; newString: string } {
     const modifiers: Set<Modifiers> = new Set();
 
-    const flaggedStringArray = (typeof input !== 'string' ? input : input.split(' ')).filter(
+    const modifiedStringArray = (typeof input !== 'string' ? input : input.split(' ')).filter(
         (value) => !value.match(/^\s*$/),
     );
 
-    let candidate = flaggedStringArray.at(0);
-    let flag = candidate ? Modifiers[candidate as keyof typeof Modifiers] : undefined;
-    while (flag !== undefined && candidate) {
-        modifiers.add(flag);
-        flaggedStringArray.shift();
-        candidate = flaggedStringArray.at(0);
-        flag = candidate ? Modifiers[candidate as keyof typeof Modifiers] : undefined;
+    let candidate = modifiedStringArray.at(0);
+    let modifier = candidate ? Modifiers[candidate as keyof typeof Modifiers] : undefined;
+    while (modifier !== undefined && candidate) {
+        modifiers.add(modifier);
+        modifiedStringArray.shift();
+        candidate = modifiedStringArray.at(0);
+        modifier = candidate ? Modifiers[candidate as keyof typeof Modifiers] : undefined;
     }
 
     return {
         modifiers: [...modifiers],
-        newString: flaggedStringArray.join(' '),
+        newString: modifiedStringArray.join(' '),
     };
 }
